5d58c81e860d78af8ad95d57bf71dfbc32786b91
Enache Mihai <enachemihai27@yahoo.com>
2024-05-27 12:10:12 +0300

    add profile info

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,62 +12,11 @@
 </head>
 <body>
 
-<nav class="navbar navbar-light bg-light ">
-    <div class="container-fluid ">
-        <a href="{{route('dashboard')}}" class="navbar-brand">Navbar</a>
-
-
-        @if (Route::has('login'))
-            <nav class="mx-3 d-flex justify-content-center align-items-center">
-                @auth
-                    <div class="items-center">
-                        <a
-                            href="{{route('dashboard')}}"
-                            class="text-decoration-none rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Dashboard
-                        </a>
-
-                            <a class="text-decoration-none btn-sm btn-info mx-1" href="{{route('client.index')}}">Client</a>
-                            <a class="text-decoration-none btn-sm btn-info mx-1" href="{{route('numbers.index')}}">Client numbers</a>
-                            <a class="text-decoration-none btn-sm btn-info mx-1" href="{{route('jobs.index')}}">Jobs</a>
-                            <a class=" text-decoration-none btn-sm btn-info mx-1" href="{{route('events.index')}}">Events</a>
-                    </div>
-                    <div>
-                        <form action="{{route('logout')}}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn-sm btn-primary">Logout</button>
-                        </form>
-                    </div>
-                @else
-                    <a
-                        href="{{ route('login') }}"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                    >
-                        Log in
-                    </a>
-
-                    @if (Route::has('register'))
-                        <a
-                            href="{{ route('register') }}"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Register
-                        </a>
-                    @endif
-                @endauth
-            </nav>
-        @endif
-
-
-
-    </div>
-</nav>
+@include('layouts.navigation')
 
 <div class="container">
     @yield('content')
 </div>
-
 
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
