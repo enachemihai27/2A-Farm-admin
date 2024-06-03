0f2459189f45d288bb4c0b7cbd4b8308b2f5bf51
Enache Mihai <enachemihai27@yahoo.com>
2024-06-03 13:55:18 +0300

    cros middl

--- app/Http/Middleware/corsMiddleware.php
+++ app/Http/Middleware/corsMiddleware.php
@@ -13,12 +13,32 @@ class corsMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
-        return $response;
+        $domains = [
+            'https://2afarm.ro',
+            'https://admin.2afarm.ro'
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if (in_array($origin, $domains)) {
+            if ($request->isMethod('OPTIONS')) {
+                return response()->json(['status' => 'success'], 200)
+                    ->header('Access-Control-Allow-Origin', $origin)
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+            }
+
+            $response = $next($request);
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+            return $response;
+        }
+
+        return $next($request);
     }
+
 }
