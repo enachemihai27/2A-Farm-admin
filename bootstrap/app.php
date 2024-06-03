320bef2f54b0e45c7308f7920a90c88ec4694b33
Enache Mihai <enachemihai27@yahoo.com>
2024-06-03 13:50:29 +0300

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->add('cors', \App\Http\Middleware\corsMiddleware::class);
+        $middleware->alias([
+            'cors' => \App\Http\Middleware\corsMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
