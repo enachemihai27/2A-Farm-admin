49a30a5e7c965b573a4eba4d5cb63c38cbe2bd9f
Enache Mihai <enachemihai27@yahoo.com>
2024-06-05 16:25:33 +0300

    add history jobs

---
+++ app/Models/History.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id','action', 'table_name', 'old_data', 'new_data'];
+
+    protected $table = 'job_table_histories';
+}
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,4 +11,38 @@ class Job extends Model
 
     protected $table = 'positions_of_employment';
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            History::create([
+                'action' => 'created',
+                'table_name' => 'positions_of_employment',
+                'user_name' => Auth()->user()->name,
+                'new_data' => json_encode($model->toArray())
+            ]);
+        });
+
+        static::updated(function ($model) {
+            History::create([
+                'action' => 'updated',
+                'table_name' => 'positions_of_employment',
+                'user_name' => Auth()->user()->name,
+                'old_data' => json_encode($model->getOriginal()),
+                'new_data' => json_encode($model->toArray())
+            ]);
+
+
+        });
+
+        static::deleted(function ($model) {
+            History::create([
+                'action' => 'deleted',
+                'table_name' => 'positions_of_employment',
+                'user_name' => Auth()->user()->name,
+                'old_data' => json_encode($model->toArray())
+            ]);
+        });
+    }
 }
